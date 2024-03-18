3b3d81c3a20ada51c0e3eb9b1208a9f5bd374ba7
Mileto Di Marco <mileto.di.marco@howest.be>
2024-03-18 09:42:46 +0100

    Add update method to productservice

--- Pri.WebApi.Core/Services/ProductService.cs
+++ Pri.WebApi.Core/Services/ProductService.cs
@@ -5,9 +5,11 @@ using Pri.CleanArchitecture.Core.Interfaces.Services;
 using Pri.CleanArchitecture.Core.Services.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Pri.CleanArchitecture.Core.Services
 {
@@ -47,12 +49,12 @@ namespace Pri.CleanArchitecture.Core.Services
                 };
             }
             //check if propertyIds exist
-            if(propertyIds.Distinct().Count() != _propertyRepository.GetAll().Where(pr => propertyIds.Contains(pr.Id)).Count())
+            if (propertyIds.Distinct().Count() != _propertyRepository.GetAll().Where(pr => propertyIds.Contains(pr.Id)).Count())
             {
                 return new ResultModel<Product>
                 {
                     IsSuccess = false,
-                    Errors = new List<string> {"Property does not exist!"}
+                    Errors = new List<string> { "Property does not exist!" }
                 };
             }
             //create the entity
@@ -119,7 +121,7 @@ namespace Pri.CleanArchitecture.Core.Services
             return new ResultModel<Product>
             {
                 IsSuccess = true,
-                Result =  product
+                Result = product
             };
         }
 
@@ -163,9 +165,67 @@ namespace Pri.CleanArchitecture.Core.Services
             };
         }
 
-        public Task<ResultModel<Product>> UpdateAsync(int id, string name, int categoryId, string description, decimal price, IEnumerable<int> propertyIds)
+        public async Task<ResultModel<Product>> UpdateAsync(int id, string name, int categoryId, string description, decimal price, IEnumerable<int> propertyIds)
         {
-            throw new NotImplementedException();
+            //check if other product with same name exists
+            if(await _productRepository.GetAll().AnyAsync(p => p.Id != id && p.Name.Equals(name)))
+            {
+                return new ResultModel<Product>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string> { "There is another product with the same name!" }
+                };
+            }
+            //check if categoryId exists
+            if (!await _categoryRepository.GetAll().AnyAsync(c => c.Id == categoryId))
+            {
+                return new ResultModel<Product>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string> { "Unknown category!" }
+                };
+            }
+            //check if propertyIds exist
+            if (propertyIds.Distinct().Count() != _propertyRepository.GetAll().Where(pr => propertyIds.Contains(pr.Id)).Count())
+            {
+                return new ResultModel<Product>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string> { "Property does not exist!" }
+                };
+            }
+            //get the product
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return new ResultModel<Product>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string> { "Product does not exist!" }
+                };
+            }
+            //update product
+            product.Name = name;
+            product.Price = price;
+            product.CategoryId = categoryId;
+            product.Properties = await _propertyRepository
+            .GetAll()
+            .Where(p => propertyIds.Contains(p.Id)).ToListAsync();
+            product.Description = description;
+            var result = await _productRepository.UpdateAsync(product);
+            if (result)
+            {
+                return new ResultModel<Product>
+                {
+                    IsSuccess = true,
+                    Result = await _productRepository.GetByIdAsync(product.Id)
+                };
+            }
+            return new ResultModel<Product>
+            {
+                IsSuccess = false,
+                Errors = new List<string> {"Product not updated!"}
+            };
         }
     }
 }
